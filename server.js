a1a5d4af8197a4f14bc9862466a36b9f8b9d2866
benjaminpettus <benjaminpettus@gmail.com>
2017-01-12 08:49:32 -0800

    working calculato

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+let app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(req, res){
+  res.render(index)
+})
+
+let server = app.listen(3000, function () {
+  console.log('Listening to port', server.address().port)
+})
